15c92a4a275d7dd5d56f3e0677314efba6477a15
rohit.kashyap <rohitkashyap2998@gmail.com>
2023-12-03 18:05:24 +0530

    comments removed

--- vendor/vendeo_management/serializers.py
+++ vendor/vendeo_management/serializers.py
@@ -8,31 +8,11 @@ class VendorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Vendor
-        # fields = ('username', 'password', 'email')  # Customize fields as needed
-        # extra_kwargs = {'password': {'write_only': True}}  # Passwo
-        # fields = '__all__'
         fields = ['vendor_name', 'vendor_address', 'vendor_contact', 'on_time_delivery_rate', 'quality_rating_avg', 'average_response_time', 'fulfillment_rate', 'vendor_code']
 
-        # fields = ( 'vendor_name','vendor_address','vendor_contact','vendor_code','id','on_time_delivery_rate')
-    # def get_vendor_contact(self, obj):
-    #     return str(obj.vendor_contact)
-    # def to_representation(self, instance):
-    #     # Access the original representation of the instance
-    #     representation = super().to_representation(instance)
-
-    #     # Ensure that float fields are converted properly to strings
-    #     float_fields = ['on_time_delivery_rate', 'quality_rating_avg', 'average_response_time', 'fulfillment_rate']
-    #     for field in float_fields:
-    #         if field in representation and isinstance(representation[field], float):
-    #             representation[field] = str(representation[field])
-    #     print('asdasd',representation)
-    #     return representation
 
 class PurchasedOrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PurchasedOrder
-        # fields = ('username', 'password', 'email')  # Customize fields as needed
-        # extra_kwargs = {'password': {'write_only': True}}  # Passwo
         fields = '__all__'
-            # fields = ['vendor_name', 'vendor_address', 'vendor_contact', 'on_time_delivery_rate', 'quality_rating_avg', 'average_response_time', 'fulfillment_rate', 'vendor_code']
