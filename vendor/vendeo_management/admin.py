cee693ebae4b1fdc7556779d1ae83a85f4e753d6
rohit.kashyap <rohitkashyap2998@gmail.com>
2023-12-05 00:26:47 +0530

    vendor_management to api

--- vendor/vendeo_management/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Vendor , PurchasedOrder
-
-admin.site.register(Vendor)
-admin.site.register(PurchasedOrder)
-
-# Register your models here.
--- vendor/vendeo_management/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VendeoManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vendeo_management'
